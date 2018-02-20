2ab3cdbc692d6becf79aef6e791bae2d64dae3ff
christofw <cwestland@hmc-heerema.com>
2018-02-20 12:44:48 +0100

    First commit. Reads headers from cryptocoincharts

---
+++ mlinc/Arb_scraper.py
@@ -0,0 +1,42 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+page = requests.get('https://cryptocoincharts.info/arbitrage')
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup(page.text, 'html.parser')
+
+# Pull all text from the BodyText div
+trade_pair_list = soup.find_all("div", {"class": "panel-heading"})
+# price_list = soup.find_all("div", {"class" : "row"})
+
+pair = []
+ex1 = []
+ex2 = []
+time = []
+
+bid = []
+ask = []
+spread = []
+volume = []
+
+for row in trade_pair_list:
+    pair.append(row.find_all('b')[0].text)
+    ex1.append(row.find_all('b')[1].text)
+    ex2.append(row.find_all('b')[2].text)
+    time.append(row.find_all('b')[3].text)
+
+# for row in price_list:
+#     print(row.find_all('arbitrage-price'))
+#     # print(bid)
+
+dataframe = pd.DataFrame(
+    {'Pair': pair,
+     'Exchange 1': ex1,
+     'Exchange 2': ex2,
+     'Time': time
+     })
+
+print(dataframe)
