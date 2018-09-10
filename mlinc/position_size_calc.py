f00eb3b70967b65e4fdb0dcbcd2251e5d2f5828e
robertw <rweegenaar@gmail.com>
2018-09-10 10:13:45 +0200

    position size calc - case #4 implemented. To Do's in script.

--- mlinc/position_size_calc.py
+++ mlinc/position_size_calc.py
@@ -18,6 +18,8 @@ output: units to trade
 import oandapyV20
 from mlinc.oanda_examples.exampleauth import exampleAuth
 
+# TODO: create if statements to determine which case is applicable, in one function.
+# TODO: After this, write this new function into the oanda trader class.
 
 def get_bid_price(instrument):
     import oandapyV20.endpoints.pricing as pricing
@@ -102,7 +104,7 @@ def get_trade_volume3(SL, current_price, balance, max_exp, inst, account_cur='EU
     max_exp: float
         percentage
     inst: basestring
-        trading instrument (e.g. GBP_USD)
+        trading instrument (e.g. EUR_GBP)
     account_cur: string
         account currency (default set to EUR)
     ----------
@@ -127,7 +129,49 @@ def get_trade_volume3(SL, current_price, balance, max_exp, inst, account_cur='EU
     return units
 
 
+def get_trade_volume4(SL, current_price, balance, max_exp, inst, account_cur='EUR'):
+    """
+    calculate trading volume with account currency  is not in the
+    trading instrument pair, but the same as the conversion pair’s base currency
+    for example: account currency is CHF and trading instrument is USD/JPY
+    The conversion pair is CHF/JPY
+    Parameters
+    ----------
+    SL
+    current_price: float
+        EUR
+    balance: float
+        account currency
+    max_exp: float
+        percentage
+    inst: basestring
+        trading instrument
+    account_cur: string
+        account currency (default set to EUR)
+    ----------
+    Returns units (int)
+    """
+    # conversion pair
+    conv_pair = account_cur+'_'+inst[4:]
+
+    # conversion pair exchange rate CHF/JPY
+    # price_conv_pair = 85.00 # baby pips example
+    price_conv_pair = float(get_bid_price(conv_pair))
+
+    # max exposure in account currency (e.g. USD)
+    max_exp_cur = balance*max_exp/100
+
+    # difference between price and SL in pips (absolute value)
+    SL_diff = 10000*abs(SL - current_price)
+
+    # for e.g. USD/JPY
+    units = round(10000 * price_conv_pair*max_exp_cur/SL_diff)
+
+    return units
+
+
 # print(get_trade_volume1(SL=12.00000, current_price=12.31031, balance=10000, max_exp=2))
 # print(get_trade_volume2(SL=1.15, current_price=1.15551, balance=10000, max_exp=2))
-print(get_trade_volume3(SL=1.27319, current_price=1.29319, balance=5000, max_exp=1, inst='EUR_GBP', account_cur='USD'))
+# print(get_trade_volume3(SL=1.27319, current_price=1.29319, balance=5000, max_exp=1, inst='EUR_GBP', account_cur='USD'))
+# print(get_trade_volume4(SL=110.049, current_price=111.049, balance=5000, max_exp=1, inst='USD_JPY', account_cur='CHF'))
 
