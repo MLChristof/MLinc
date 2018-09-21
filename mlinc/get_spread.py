d92cd18187d22ee6be3197a0d1c9e870f8884615
robertw <rweegenaar@gmail.com>
2018-09-21 10:22:28 +0200

    get spread updated

--- mlinc/get_spread.py
+++ mlinc/get_spread.py
@@ -5,19 +5,27 @@ from datetime import datetime
 
 
 # Example script for retrieving spread on given instrument
-
+# TODO: easier to get ask and bid price and subtract. No if else statements needed
 def get_spread():
     # see http://developer.oanda.com/rest-live/forex-labs/#spreads
-    inst = 'XCU_USD'
+    inst = 'DE30_EUR'
     accountID, access_token = exampleAuth()
     api = oandapyV20.API(access_token=access_token)
 
     params = {
         "instrument": inst,
         "period": 1
-    }
+             }
     r = labs.Spreads(params=params)
     api.request(r)
-    return r.response
+    if 'JPY' in inst:
+        spread = format(r.response['avg'][0][1] / 1, '.3f')
+    elif 'BCO' in inst:
+        spread = format(r.response['avg'][0][1] / 100, '.3f')
+    elif 'DE30' in inst:
+        spread = format(r.response['avg'][0][1] / 1, '.1f')
+    else:
+        spread = format(r.response['avg'][0][1] / 10000, '.5f')
+    return spread
 
-print(str(get_spread()['avg'][0][1]/10000))
+print(get_spread())
