556413d6d06619a9c4b28f3df6f5d6b5167b1676
robertw <rweegenaar@gmail.com>
2018-09-22 19:02:40 +0200

    spread from bid and ask.

--- mlinc/get_spread.py
+++ mlinc/get_spread.py
@@ -2,30 +2,23 @@ import oandapyV20.endpoints.forexlabs as labs
 from mlinc.oanda_examples.exampleauth import exampleAuth
 import oandapyV20
 from datetime import datetime
-
+from mlinc.oanda_examples.candle_data import candles
 
 # Example script for retrieving spread on given instrument
-# TODO: easier to get ask and bid price and subtract. No if else statements needed
-def get_spread():
-    # see http://developer.oanda.com/rest-live/forex-labs/#spreads
-    inst = 'DE30_EUR'
+
+
+def get_spread(inst):
     accountID, access_token = exampleAuth()
     api = oandapyV20.API(access_token=access_token)
-
-    params = {
-        "instrument": inst,
-        "period": 1
-             }
-    r = labs.Spreads(params=params)
-    api.request(r)
-    if 'JPY' in inst:
-        spread = format(r.response['avg'][0][1] / 1, '.3f')
-    elif 'BCO' in inst:
-        spread = format(r.response['avg'][0][1] / 100, '.3f')
-    elif 'DE30' in inst:
-        spread = format(r.response['avg'][0][1] / 1, '.1f')
-    else:
-        spread = format(r.response['avg'][0][1] / 10000, '.5f')
+    test = candles(inst, granularity=['M1'], count=[1], From=None, to=None, price=['BA'], nice=True, access_token=access_token)
+    bid = float(test['candles'][0]['bid']['c'])
+    ask = float(test['candles'][0]['ask']['c'])
+    print(bid)
+    print(ask)
+    spread = format(ask-bid, '.5f')
     return spread
 
-print(get_spread())
+
+if __name__ == "__main__":
+    test = get_spread(inst=['XAU_CAD'])
+    print(test)
