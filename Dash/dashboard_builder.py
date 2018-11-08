14f6d0db9a2bb0aadf687f3f6cbb5f23b7e6651c
Christof <christof.westland@gmail.com>
2018-11-08 20:13:20 +0100

    First commit dashboard. Working example.

---
+++ Dash/dashboard_builder.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='MLinc Thunder Dashboard'),
+
+    html.Div(children='''
+        Making billions and billions and billions.
+    '''),
+
+    dcc.Dropdown(
+        options=[
+            {'label': 'Fritsie op de daily', 'value': 'daily'},
+            {'label': 'Fritsie op de hourly', 'value': 'hourly'}
+        ],
+        value='daily'
+    ),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
