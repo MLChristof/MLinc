ffc7de50a5c975ba5a090e731cacc433175e5ef9
christofw <christof.westland@gmail.com>
2018-11-09 13:14:21 +0100

    Minor change

--- Dash/dashboard_builder.py
+++ Dash/dashboard_builder.py
@@ -2,6 +2,10 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from mlinc import oanda_trader
+from mlinc import oanda_examples
+
+trader = oanda_trader.OandaTrader(object)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -26,7 +30,7 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
