1ccb8b05de0e42adda7055d61fd6f51f1432aae9
christofw <christof.westland@gmail.com>
2018-11-13 11:53:12 +0100

    Nu werkt t echt

--- Dash/dashboard_builder.py
+++ Dash/dashboard_builder.py
@@ -3,24 +3,40 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from mlinc import oanda_trader
-from datetime import datetime
+import configparser
 
-trader = oanda_trader.OandaTrader.from_conf_file('all',
-                                                 r'C:\Data\Documents\Christof\Python\Trading\MLinc\mlinc\conf.ini')
 
-trade_data = trader.get_all_trades()
-account_balance = trader.account_balance()
+def from_conf_file(instruments, conf):
+    config = configparser.RawConfigParser(allow_no_value=True)
+    config.read(conf)
+    input = {}
+    for item in config['OandaTraderInput']:
+        input[item] = config['OandaTraderInput'][item]
 
-trade_data['balance'] = 0
+    try:
+        assert input['strategy']
+        strategy = input['strategy']
+        if strategy in ['Baconbuyer', 'Inverse_Baconbuyer']:
+            pass
+        else:
+            raise ValueError(f'Strategy not possible...')
+    except AssertionError:
+        raise ValueError(f'Please provide a strategy in: {conf}')
 
-balance = account_balance
-for idx in trade_data.index:
-    balance -= trade_data.iloc[idx+1]['realizedPL']
-    trade_data.at[idx, 'balance'] = balance
+    return input
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+account_id = ['101-004-7108173-001',
+              '101-004-7108173-002',
+              '101-004-7108173-003',
+              '101-004-7108173-004',
+              '101-004-7108173-005',
+              '101-004-7108173-006',
+              '101-004-7108173-007']
 
 app.layout = html.Div(children=[
     html.H1(children='MLinc Thunder Dashboard'),
@@ -30,47 +46,65 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Dropdown(
-        options=[
-            {'label': 'Fritsie op de daily', 'value': 'daily'},
-            {'label': 'Fritsie op de hourly', 'value': 'hourly'}
-        ],
-        value='daily'
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in account_id],
+        value=account_id[0]
     ),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': trade_data['closeTime'], 'y': trade_data['realizedPL'], 'type': 'line', 'name': 'PL'},
-                {'x': trade_data['closeTime'], 'y': trade_data['balance'], 'type': 'line', 'name': 'Balance','yaxis': 'y2'}
-            ],
-            'layout': {
-                'title': 'Account balance history',
-                'yaxis': dict(
-                    title='Realized PL',
-                    titlefont=dict(
-                        color='#1f77b4'
-                    ),
-                    tickfont=dict(
-                        color='#1f77b4'
-                    )
+    dcc.Graph(id='ML_dashboard')
+    ])
+
+
+@app.callback(
+    dash.dependencies.Output('ML_dashboard', 'figure'),
+    [dash.dependencies.Input('dropdown', 'value')])
+def update_figure(selected_account):
+    conf_input = from_conf_file('all', r'C:\Data\Documents\Christof\Python\Trading\MLinc\mlinc\conf.ini')
+    conf_input['accountid'] = selected_account
+
+    trader = oanda_trader.OandaTrader('all', **conf_input)
+
+    trade_data = trader.get_all_trades()
+    account_balance = trader.account_balance()
+
+    trade_data['balance'] = 0
+
+    balance = account_balance
+    for idx in trade_data.index:
+        balance -= trade_data.iloc[idx]['realizedPL']
+        trade_data.at[idx, 'balance'] = balance
+
+    return {
+        'data': [
+            {'x': trade_data['closeTime'], 'y': trade_data['realizedPL'], 'type': 'line', 'name': 'PL'},
+            {'x': trade_data['closeTime'], 'y': trade_data['balance'], 'type': 'line', 'name': 'Balance','yaxis': 'y2'}
+        ],
+        'layout': {
+            'title': 'Account balance history',
+            'yaxis': dict(
+                title='Realized PL',
+                titlefont=dict(
+                    color='#1f77b4'
                 ),
-                'yaxis2': dict(
-                    title='Balance',
-                    titlefont=dict(
-                        color='#d62728'
-                    ),
-                    tickfont=dict(
-                        color='#d62728'
-                    ),
-                    anchor='x',
-                    overlaying='y',
-                    side='right'
+                tickfont=dict(
+                    color='#1f77b4'
+                )
+            ),
+            'yaxis2': dict(
+                title='Balance',
+                titlefont=dict(
+                    color='#d62728'
                 ),
-            }
+                tickfont=dict(
+                    color='#d62728'
+                ),
+                anchor='x',
+                overlaying='y',
+                side='right'
+            ),
         }
-    )
-])
+    }
+
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
