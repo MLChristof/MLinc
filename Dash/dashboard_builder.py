571ae7657c42adb88ffd64b172970190c92fd87d
JELLETB <jterbraak@hmc-heerema.com>
2018-12-12 11:04:58 +0100

    small update

--- Dash/dashboard_builder.py
+++ Dash/dashboard_builder.py
@@ -66,8 +66,9 @@ app.layout = html.Div(children=[
 @app.callback(
     dash.dependencies.Output('ML_dashboard', 'figure'),
     [dash.dependencies.Input('dropdown', 'value')])
+
 def update_figure(selected_account):
-    conf_input = from_conf_file('all', r'C:\Data\Documents\Christof\Python\Trading\MLinc\mlinc\conf.ini')
+    conf_input = from_conf_file('all', r'C:\Data\2_Personal\Python_Projects\MLinc\mlinc\conf.ini')
     conf_input['accountid'] = selected_account
 
     trader = oanda_trader.OandaTrader('all', **conf_input)
