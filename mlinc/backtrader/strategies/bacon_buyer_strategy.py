65b0cd5dbd354ab1a3e4e4e7dbaed5ae2e6c18c0
Robbie <rweegenaar@gmail.com>
2019-01-18 23:09:02 +0100

    ma period updated

--- mlinc/backtrader/strategies/bacon_buyer_strategy.py
+++ mlinc/backtrader/strategies/bacon_buyer_strategy.py
@@ -14,7 +14,7 @@ class BaconBuyerStrategy(bt.Strategy):
     # TODO Add smart staking/sizing
     # TODO Check Commission settings
     params = (
-        ('maperiod', 48),
+        ('maperiod', 10),
         ('RRR', 5),
         ('minSL', 2),  # in pips
         ('stakepercent', 5)
