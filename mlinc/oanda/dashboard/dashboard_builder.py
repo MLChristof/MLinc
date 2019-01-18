e17b9640829e77a099f59607fda88acd8151f96d
Robbie <rweegenaar@gmail.com>
2019-01-18 20:41:07 +0100

    import trader updated

--- mlinc/oanda/dashboard/dashboard_builder.py
+++ mlinc/oanda/dashboard/dashboard_builder.py
@@ -2,7 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from mlinc.oanda import oanda_trader
+from mlinc.oanda import trader
 import configparser
 import base64
 
