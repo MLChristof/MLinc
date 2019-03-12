369a55430ce91f7876f2e6076a32b34840b106bf
JELLETB <jterbraak@hmc-heerema.com>
2019-03-12 12:50:37 +0100

    fixt notifier

--- mlinc/trump2cash/trump_notifier.py
+++ mlinc/trump2cash/trump_notifier.py
@@ -5,7 +5,13 @@ from mlinc.notifier import notification, file_robert, file_christof, file_jelle,
 
 twitter_api = Twitter()
 
-tweets = twitter_api.get_tweets('1085519375224983552')
+# test_id = '1085519375224983552'
+
+with open('last_id.txt', 'r') as f:
+    last_tweet_id = f.read()
+
+tweets = twitter_api.get_tweets(last_tweet_id)
+# tweets = twitter_api.get_tweets(test_id)
 
 for i in tweets:
     text = i['full_text']
@@ -18,21 +24,15 @@ for i in tweets:
 
     message = f'Date = {time} \n {text} \n Nouns: {nouns} \n Polarity = {polarity}, Subjectivity = {subjectivity}'
 
-    print(message)
+    if abs(polarity) > 0.8:
+        notification(fileID=file_christof, message=message)
+        notification(fileID=file_jelle, message=message)
+        notification(fileID=file_robert, message=message)
+        notification(fileID=file_vincent, message=message)
 
-    # notification(fileID=file_christof, message=message)
-    # notification(fileID=file_vincent, message=message)
+with open('last_id.txt', 'w') as f:
+    f.write(str(tweets[0]['id']))
 
-
-
-    #
-    # text = TextBlob(tweet)
-    # newsentiment = text.sentiment
-    #
-    # print tweet
-    # print text.tags
-    # print text.noun_phrases
-    # print newsentiment
 
 
 
