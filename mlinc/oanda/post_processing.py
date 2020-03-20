0eb50492852a4f909970063cd9b32f3651edd8eb
robertw <rweegenaar@gmail.com>
2020-03-20 09:20:14 +0100

    results save to csv

---
+++ mlinc/oanda/post_processing.py
@@ -0,0 +1,23 @@
+from mlinc.oanda.trader import OandaTrader
+import configparser
+
+
+def from_conf_file(instruments, conf):
+    config = configparser.RawConfigParser(allow_no_value=True)
+    config.read(conf)
+    input = {}
+    for item in config['OandaTraderInput']:
+        input[item] = config['OandaTraderInput'][item]
+
+    return input
+
+
+if __name__ == '__main__':
+
+    conf_input = from_conf_file('all', r'C:\Data\2_Personal\Python_Projects\MLinc\mlinc\oanda\conf_files\conf.ini')
+
+    trader = OandaTrader('all', **conf_input)
+
+    trades = trader.get_all_trades()
+    trades.to_csv(r'C:\Data\2_Personal\Python_Projects\MLinc\mlinc\oanda\results\xcu20032020.csv')
+
